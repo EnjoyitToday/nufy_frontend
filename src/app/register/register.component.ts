dd2ea26bd88f7e08b1fe118c167065c4ce5dc409
Pedro Duarte <pcduarte@edu.unifor.br>
2023-05-28 16:21:57 -0300

    feat: Adiciona validador de form

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
+import { FormError } from '../shared/types/formError';
 import { RegisterService } from './register.service';
 
 @Component({
@@ -10,6 +11,7 @@ import { RegisterService } from './register.service';
 export class RegisterComponent implements OnInit {
 
   public form: FormGroup
+  public errors: FormError[]
 
   constructor(
     public formBuilder: FormBuilder,
@@ -18,10 +20,40 @@ export class RegisterComponent implements OnInit {
 
   ngOnInit(): void {
     this.form = this.formBuilder.group({
-      username: [''],
-      password: [''],
-      email: [''],
-      email_confirm: [''],
+      username: [{
+        value: '',
+        disabled: false,
+      }, [
+        Validators.required,
+      ]],
+      password: [{
+        value: '',
+        disabled: false,
+      }, [
+        Validators.required,
+      ]],
+      password_confirm: [{
+        value: '',
+        disabled: false,
+      }, [
+        Validators.required,
+        this.mismatchedFields('password', 'mismatched_password')
+      ]],
+      email: [{
+        value: '',
+        disabled: false,
+      }, [
+        Validators.email,
+        Validators.required,
+      ]],
+      email_confirm: [{
+        value: '',
+        disabled: false,
+      }, [
+        Validators.email,
+        Validators.required,
+        this.mismatchedFields('password', 'mismatched_email')
+      ]],
     })
   }
 
@@ -33,14 +65,87 @@ export class RegisterComponent implements OnInit {
   }
 
   async submit() {
-    const username = this.form.get('username')?.value
-    const password = this.form.get('password')?.value
-    const email = this.form.get('email')?.value
-    const email_confirm = this.form.get('email_confirm')?.value
-    if (email != email_confirm) {
+    if (this.form.valid) {
+      const username = this.form.get('username')?.value
+      const password = this.form.get('password')?.value
+      const password_confirm = this.form.get('password_confirm')?.value
+      const email = this.form.get('email')?.value
+      const email_confirm = this.form.get('email_confirm')?.value
+      if (email != email_confirm) {
+        return
+      }
+      await this.registerService.cadasterUser(username, password, email);
+
       return
     }
-    await this.registerService.cadasterUser(username, password, email);
 
+    this.errors = this.getFormValidationErrors(this.form)
+    console.log(this.errors)
   }
+
+  getFormValidationErrors (form: FormGroup): FormError[] {
+    const result: FormError[] = [];
+    Object.keys (form.controls).forEach (key => {
+      const controlErrors: ValidationErrors|null|undefined = form.get (key)?.errors;
+      if (controlErrors) {
+        Object.keys (controlErrors).forEach (keyError => {
+          result.push ({
+            control: key,
+            error: keyError,
+            value: controlErrors [keyError],
+            humanMessage: this.getHumanMessage(keyError, key)
+          });
+        });
+      }
+    });
+
+    return result;
+  }
+
+  getHumanMessage(error: string, key: string): string {
+    const input = this.getFormattedControlName(key)
+    const errorMessages: { [key: string]: string } = {
+      email: 'E-mail inválido por favor digite um e-mail válido, exemplo: example@example.com.',
+      required: `O campo ${input} é obrigatório.`,
+      mismatched_email: 'Os emails não são iguais.',
+      mismatched_password: 'As senhas não são iguais.'
+    }
+
+    return errorMessages[error]
+  }
+
+  getFormattedControlName(control: string): string {
+    const errorMessages: { [key: string]: string } = {
+      username: 'usuário',
+      password: 'senha',
+      password_confirm: 'confirmar senha',
+      email: 'e-mail',
+      email_confirm: 'confirmar e-mail'
+    }
+
+    return errorMessages[control]
+  }
+
+  mismatchedFields(passwordInput: string, errorKey: string): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: unknown } => {
+      const parent = control.parent;
+      if (!parent) {
+        return {}
+      }
+
+      const otherControl = parent.get(passwordInput)
+      const otherControlValue = otherControl?.value as string
+      const actualControlValue = control.value as string
+      if (otherControlValue !== actualControlValue) {
+        return {
+          [errorKey]: true,
+        }
+      }
+
+      return {}
+    }
+  }
+
+
+
 }
